162a351383c7831e405c34a61b5119144ba557ad
Stan Cosmin Ioan <stancosminioan@gmail.com>
2025-08-21 11:17:21 +0300

    [DONE] - add icons

--- src/Components/EuroProjects.jsx
+++ src/Components/EuroProjects.jsx
@@ -1,18 +1,21 @@
 import euroProjects from "../Model/euroProjects";
 import { Link } from "react-router-dom";
+import data from "../Model/data";
+
+const { projectIcon } = { ...data };
 
 const EuroProjectCard = ({ title, byline, id }) => (
   <Link
     to={`/fragar/noutati/${id}`}
-    className="w-full h-64 bg-[#CAE7FF] rounded-lg flex flex-col justify-center items-center text-center p-4 lg:p-6"
+    className="w-full h-auto bg-[#CAE7FF] rounded-2xl flex flex-col justify-center items-center text-center p-4 lg:p-20"
   >
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-2">
       <div className="w-12 h-12 bg-gray-300 rounded flex items-center justify-center mb-4">
-        <img src={euroProjects[0].projectIcon} alt="📄" />{" "}
+        <img src={projectIcon} alt="📄" />{" "}
       </div>
       <p className="text-gray-700 text-sm lg:text-base">{title}</p>
       <p className="text-gray-500 text-xs lg:text-sm mt-1">{byline}</p>
-      <button className="mt-4 py-2 px-4 border-2 border-gray-700 rounded-lg text-gray-700 text-sm lg:text-base">
+      <button className="cursor-pointer hover:scale-103 transform duration-200 mt-4 py-2 px-4 border-2 border-gray-700 rounded-lg text-gray-700 text-sm lg:text-base">
         Vezi tot
       </button>
     </div>
