3ebc2943148df5d92bef6e21c78d27a7a7e6fe69
Stan Cosmin Ioan <stancosminioan@gmail.com>
2025-08-25 14:53:56 +0300

    [DONE] - fix media final

--- src/Model/euroProjects.js
+++ src/Model/euroProjects.js
@@ -4,7 +4,8 @@ const euroProjects = [
     title:
       "Lansare proiect / Finanțare nerambursabilă pentru SC FRAGAR TRADING SRL DIGI TOUCH NEXTGEN",
     // euroProjects: "/public/fragar_project.jpeg",
-    euroProjects: "./public/euro_project_fragar_logos.jpeg",
+    euroProjects:
+      "https://stancosmin28.github.io/fragar/euro_project_fragar_logos.jpeg",
     byline: "by admin | Iulie 28, 2025",
     paragraphs: [
       "SC FRAGAR TRADING SRL, cu sediul în Iași, Str. Aleea Rozelor, nr. 6A, Demisol, în parteneriat cu UNIVERSITATEA „ȘTEFAN CEL MARE” DIN SUCEAVA, anunță începerea activităților proiectului „DIGI TOUCH NEXTGEN”, Cod SMIS 338580.",
